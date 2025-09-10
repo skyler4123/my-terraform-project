0fe2a1edab40c2a192fc3b7151af06745db1fdd1
skyler <pkphung2001@gmail.com>
2025-09-11 00:22:17 +0700

    minor

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name-123" # Must be globally unique
+  tags = {
+    Name = "MyFirstBucket"
+  }
+}
